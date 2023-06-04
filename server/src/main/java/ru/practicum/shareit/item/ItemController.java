7e9017d198ddd77f2acc59d7aa0ff309e0464b46
RybkinMike <Misha.tes-03@mail.ru>
2023-06-04 12:02:35 +0300

    refactore: реализовано ТЗ-16

---
+++ server/src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -0,0 +1,69 @@
+package ru.practicum.shareit.item;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.model.Comment;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.item.model.ItemService;
+import ru.practicum.shareit.item.model.ItemWithBooking;
+import java.util.Collection;
+import java.util.List;
+
+@RestController
+@RequestMapping("/items")
+@RequiredArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+public class ItemController {
+    ItemService itemService;
+    static final String USERID = "X-Sharer-User-Id";
+
+    @GetMapping
+    public Collection<ItemWithBooking> get(@RequestHeader(USERID) long userId,
+                                           @RequestParam(value = "from", defaultValue = "0") int from,
+                                           @RequestParam(value = "size", defaultValue = "10") int size) {
+        return itemService.getItems(userId, from, size);
+    }
+
+    @GetMapping("/{itemId}")
+    public ItemWithBooking getById(@RequestHeader(USERID) long userId,
+                           @PathVariable long itemId) {
+        return itemService.getItemById(userId, itemId);
+    }
+
+    @GetMapping("/search")
+    public List<ItemDto> getById(@RequestParam(name = "text") String query,
+                                 @RequestParam(value = "from", defaultValue = "0") int from,
+                                 @RequestParam(value = "size", defaultValue = "10") int size) {
+
+        return itemService.getItemByQuery(query, from, size);
+    }
+
+    @PostMapping
+    public Item add(@RequestHeader(USERID) long userId,
+                    @RequestBody ItemDto itemDto) {
+        return itemService.addNewItem(userId, itemDto);
+    }
+
+    @PostMapping("/{itemId}/comment")
+    public Comment addComment(@RequestHeader(USERID) long userId,
+                           @RequestBody Comment comment,
+                           @PathVariable long itemId) {
+        return itemService.addNewComment(userId, comment, itemId);
+    }
+
+    @PatchMapping("/{itemId}")
+    public ItemDto update(@RequestHeader(USERID) long userId,
+                          @RequestBody ItemDto itemDto,
+                          @PathVariable long itemId) {
+        return itemService.updateItem(userId, itemDto, itemId);
+    }
+
+    @DeleteMapping("/{itemId}")
+    public void deleteItem(@RequestHeader(USERID) long userId,
+                           @PathVariable long itemId) {
+        itemService.deleteItem(userId, itemId);
+    }
+}
---
+++ server/src/main/java/ru/practicum/shareit/user/UserController.java
@@ -0,0 +1,40 @@
+package ru.practicum.shareit.user;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/users")
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping
+    public User saveNewUser(@RequestBody User user) {
+            return userService.saveUser(user);
+    }
+
+    @PatchMapping("/{userId}")
+    public User update(@RequestBody User user, @PathVariable long userId) {
+        log.info("Запрос на обновление данных пользователя {}", user);
+        return userService.update(userId, user);
+    }
+
+    @GetMapping("/{userId}")
+    public User getById(@PathVariable long userId) {
+        return userService.getById(userId);
+    }
+
+    @DeleteMapping("/{userId}")
+    public void delete(@PathVariable long userId) {
+        userService.delete(userId);
+    }
+}
